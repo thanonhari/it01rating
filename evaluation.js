4474c1a533dbfbc44bb3b979edb536917b3b2a03
thanonhari <55010983+thanonhari@users.noreply.github.com>
2025-09-13 16:10:44 +0700

    Update evaluation.js

--- evaluation.js
+++ evaluation.js
@@ -1,8 +1,8 @@
 // ==========================================================
 // == ✨ กรุณาแก้ไขค่า 2 บรรทัดนี้ ✨ ==
 // ==========================================================
-const LIFF_ID = "2008092354-pgKAqJ8O"; // LIFF ID ของหน้าประเมินที่คุณสร้างใหม่
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwFzsPVx7P8srOQZzTC57PqUY9ePm_L5pdBVAXKEsHOJt5LEFLy1REYxIG-O89i-hY3/exec"; // URL ของ Web App ที่ได้จากการ Deploy
+const LIFF_ID = "2007495650-QYp0MBBk"; // LIFF ID ของหน้าประเมินที่คุณสร้างใหม่
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycby02fO2_GgLEWJif2dYzF9wKK1f9SDiRxXGPuCshwIdwx-e4MZj2227RZ8-fSJCZm46GQ/exec"; // URL ของ Web App ที่ได้จากการ Deploy
 // ==========================================================
 
 // =====================================================================
